9465b8cdf4daf854d288d96d3900cfc5ea4cd2f1
rudix <127332134+rud1x@users.noreply.github.com>
2026-09-15 17:19:32 +0300

    Update NeoShell.py

--- NeoShell.py
+++ NeoShell.py
@@ -407,10 +407,6 @@ class NeoShellHandler(BaseHTTPRequestHandler):
 
     def do_OPTIONS(self):
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type, X-Device-Key, Authorization')
-        self.send_header('Access-Control-Max-Age', '86400')
         self.end_headers()
 
     def do_GET(self):
